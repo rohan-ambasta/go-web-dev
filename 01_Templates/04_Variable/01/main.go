a8a793e2ea2589af4b522c685c5c9916c8da5440
rohan-ambasta <ambasta.rohan@gmail.com>
2017-03-28 19:12:51 +0530

    Using variable in templates

---
+++ 01_Templates/04_Variable/01/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+
+}
+
+// *****************************************
+// You get to pass in one value - that's it!
+// *****************************************
+func main() {
+	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", `Release self-focus; embrace other-focus.`)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+}
